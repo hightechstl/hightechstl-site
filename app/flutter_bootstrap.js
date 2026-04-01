e1e714a80e7eb385fe6a4840b9b3d12b8705e5c0
hightechstl <hightechstl@users.noreply.github.com>
2026-04-01 04:11:51 -0500

    deploy: daily shop rotation + rarity system (test mode: all items free/lvl1)

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3676522857" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "788822795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
